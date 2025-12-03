cc9d9c8e7b4ea01d8fa85c74fe4f1fa8b178afb3
alaa781 <habibaabobasha@gmail.com>
2025-12-03 21:21:06 +0200

    deleting listen to server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(process.env.PORT,()=>{
-   console.log("server is running at:",`http://localhost:${process.env.PORT}`);
-})  
+// app.listen(process.env.PORT,()=>{
+//    console.log("server is running at:",`http://localhost:${process.env.PORT}`);
+// })  
