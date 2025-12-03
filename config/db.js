698a9fe6d843ba5f557dcc99ccd4a40f0c7326e5
alaa781 <habibaabobasha@gmail.com>
2025-12-03 22:05:58 +0200

    adding serverless handler

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+  const mongoose = require("mongoose");
+  require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("connected to mongoDB server successfully");
-  } catch (err) {
-    console.log(`failed to connect with mongoDB server: ${err}`);
-  }
-};
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("connected to mongoDB server successfully");
+    } catch (err) {
+      console.log(`failed to connect with mongoDB server: ${err}`);
+    }
+  };
 
-module.exports = connectDB;
+  module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,8 +2,20 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-connectDB();
+//connectDB();
 
 // app.listen(process.env.PORT,()=>{
 //    console.log("server is running at:",`http://localhost:${process.env.PORT}`);
 // })  
+
+// Serverless handler
+let conn = null;
+
+module.exports = async (req, res) => {
+  // Connect to MongoDB only once
+  if (!conn) {
+    conn = await connectDB();
+  }
+  // Pass request to Express app
+  return app(req, res);
+};
